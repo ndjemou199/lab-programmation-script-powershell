501efcc5afe7e9a20a2affc5fbbc432e0b567093
franck1999-30 <christinnguekam@gmail.com>
2020-04-07 15:01:41 -0400

    break time :bread:

--- b000000000-structure.ps1
+++ b000000000-structure.ps1
@@ -6,9 +6,9 @@
     Ce script est utilisé pour le laboratoire de programmation en Powershell.
 
 .NOTES
-    Author: CollegeBoreal
+    Author: 300118196
     Derniere mise à jour: yyyy-mm-dd
 
 #>
 
-Write-Host "Bonjour Monde"
+Write-Host "Bonjour $personneNom = "Alice", tu as $personneAge = 35 ans"

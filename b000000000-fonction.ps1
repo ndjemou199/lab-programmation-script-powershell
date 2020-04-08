d1eee0419d5c45d770451ddeb45e4535c84468f5
franck1999-30 <christinnguekam@gmail.com>
2020-04-08 15:31:18 -0400

    Tutoriel complété

--- b000000000-fonction.ps1
+++ b000000000-fonction.ps1
@@ -13,14 +13,18 @@
 
 # Definition de la fonction
 function Stagiaire {
-param(
-[string]$personneNom,
-[Int]$personneAge
-)
-"Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge
 
+[CmdletBinding()]
+param(
+    [string]$personneNom,
+    [Int]$personneAge
+)
+#message de bienvenue
+BEGIN {Write-Verbose "Début du script"}
+PROCESS {"Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge}
+END {Write-Verbose "Fin du script"}
 }
 
 # Appel de la fonction
-Stagiaire  Toronto 35
+Stagiaire  "Pascal Siakam" 26 -Verbose
 
